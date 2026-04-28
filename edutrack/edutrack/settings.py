f79be58d5c3e4f309e9d394ca1fbee4231123519
User <user@example.com>
2026-04-28 21:50:04 +0530

    changed logo in app.json

--- edutrack/edutrack/settings.py
+++ edutrack/edutrack/settings.py
@@ -41,7 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    'cloudinary',
     'rest_framework',
     "drf_spectacular", 
     'accounts',
@@ -180,7 +182,14 @@ USE_I18N = True
 
 USE_TZ = True
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# DEFAULT_FILE_STORAGE is for uploaded media files
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
 
 
 
--- student/app/config.ts
+++ student/app/config.ts
@@ -1,3 +1,3 @@
 // Automatically use the EXPO_PUBLIC_API_URL environment variable if set.
 // Otherwise, fallback to the local development IP.
-export const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL || "http://10.25.224.125:8000";
+export const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL || "https://edutrack-1vq9.onrender.com";
